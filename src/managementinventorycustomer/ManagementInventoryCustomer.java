73aed4d4e8c333110ca83b1cd9ae4a9430cfa797
pegaso02 <fabianmanrique10@gmail.com>
2024-09-11 19:44:22 -0500

    Logica

---
+++ src/managementinventorycustomer/ManagementInventoryCustomer.java
@@ -0,0 +1,27 @@
+
+package managementinventorycustomer;
+
+import Vista.Login;
+import Vista.Register;
+
+
+
+
+public class ManagementInventoryCustomer {
+
+    
+    public static void main(String[] args) {
+       
+      
+//        Register register = new Register();
+//        register.setVisible(true);
+//        register.setLocale(null);
+
+        Login login = new Login();
+        login.setVisible(true);
+        
+        
+        
+    }
+    
+}
